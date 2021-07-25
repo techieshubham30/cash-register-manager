8fc83c73228480e8f4190da9ee12c7bf9d87a8ff
techieshubham30 <82032825+techieshubham30@users.noreply.github.com>
2021-07-25 16:59:12 +0530

    initial commit

--- app.js
+++ app.js
@@ -14,39 +14,33 @@ var cnt_20=0;
 var cnt_10=0;
 var cnt_5=0;
 var cnt_1=0;
+if(res>0){
 
     while(res!=0){
         if(res>=2000){
             cnt_2000+=Math.floor(res/2000);
             res=res%2000;
-        }
-        if(res>=500){
+        }else if(res>=500){
             cnt_500+=Math.floor(res/500);
             res=res%500;
-        }
-
-        if(res>=100){
+        }else if(res>=100){
             cnt_100+=Math.floor(res/100);
             res=res%100;
-        }
-        if(res>=20){
+        }else if(res>=20){
             cnt_20+=Math.floor(res/20);
             res=res%20;
-        }
-        if(res>=10){
+        }else if(res>=10){
             cnt_10+=Math.floor(res/10);
             res=res%10;
-        }
-        if(res>=5){
+        }else if(res>=5){
             cnt_5+=Math.floor(res/5);
             res=res%5;
-        }
-
-        if(res>=1){
-            cnt_1+=Math.floor(res/2);
-            res=res%2;
+        }else if(res>=1){
+            cnt_1+=Math.floor(res/1);
+            res=res%1;
         }
     }
+}
 
     var notesOF2000=document.querySelector('.noOfNotes2000');
     var notesOF500=document.querySelector('.noOfNotes500');
@@ -63,6 +57,5 @@ var cnt_1=0;
        notesOF10.innerText=cnt_10;
        notesOF5.innerText=cnt_5;
        notesOF1.innerText=cnt_1;
-
 
 }
